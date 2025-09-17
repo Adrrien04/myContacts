a89edff8690b7ff477442c59cdd7cfc9cc19fb1c
Adrrien04 <adrrien.chandrakumar@gmail.com>
2025-09-17 11:41:19 +0200

    can connect to the DB

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Connecté'))
+    .catch(err => console.error('Erreur:', err));
+
+app.get('/', (req, res) => {
+    res.send('Backend Express & Mongoose fonctionne 🚀');
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur sur ${PORT}`);
+});
