691664170cb08028a561edefd2537f0974c2a432
Adrrien04 <adrrien.chandrakumar@gmail.com>
2025-10-06 16:50:36 +0200

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
+// backend/server.js
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./route/auth.js";
 import contactRoutes from "./route/contact.js";
 import swaggerUi from "swagger-ui-express";
@@ -13,13 +16,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connecté"))
   .catch((err) => console.error("Erreur:", err));
 
